c5d95f4c3f424ea64b1e346bcfe199bc77bf5d90
Rosiel Victor <codepremium917@gmail.com>
2023-08-16 17:42:51 -0300

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# <center>ALL DEVS</center>
+### <center>Nós somos o All Devs, um grupo de desenvolvedores que fazem o que <s>amam</s> dá pra fazer.</center>
+
+## Quem somos?
+Somos um grupo de desenvolvedores que estão espalhados pelo Brasil, e que se juntaram para fazer projetos e aprender juntos.
+
+Inicialmente, tinhamos sonhos de criar coisas que ajudassem pessoas e que fossem úteis para a sociedade, mas como a maioria dos sonhos, eles foram quebrados e agora só queremos fazer coisas que nos dêem dinheiro. (brincadeira, ainda queremos ajudar as pessoas)
+
+Beleza, mas o que vocês fazem?
+- Sites
+- Aplicativos
+- Jogos
+- Bots
+- Dançamos Arrocha
+- E muito mais!
+
+## Projetos
+### Casa dos Anjos
+Criamos um aplicativo que visa ajudar a ONG Casa dos Anjos, que cuida de animais abandonados e maltratados. O aplicativo tem como objetivo arrecadar dinheiro para a ONG, e também ajudar a conscientizar as pessoas sobre a importância de cuidar dos animais.
+
+- [Baixe o aplicativo](https://play.google.com/store/apps/details?id=com.casadosanjos) (Android)
+- Baixe o aplicativo (iOS) - Em breve
+- [Repositório do aplicativo](https://github.com/misterioso013/CasaDosAnjos)
+- [Conheça a ONG](https://www.instagram.com/casadosanjos/)
---
+++ src/app/page.tsx
@@ -0,0 +1,92 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm uppercase lg:flex">
+        <a href="https://t.me/AllDevsBR" target="_blank" rel="noopener noreferrer">
+          @AllDevsBR
+        </a>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          {'<3'}
+        </div>
+      </div>
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <h1
+          className="relative z-10 text-6xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-br from-slate-400 to-slate-900 dark:from-zinc-500 dark:to-zinc-50 lg:text-8xl"
+        >
+          All Devs
+        </h1>
+      </div>
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://github.com/misterioso013/public-apis"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            APIs{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Build your own applications with our APIs
+          </p>
+        </a>
+
+        <a
+          href="https://github.com/misterioso013/alldevs"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            About Us{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Who are we? What do we do? Why do we do it?
+          </p>
+        </a>
+
+        <a
+          href="https://github.com/misterioso013/alldevs/projects"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Projects{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            See our projects and contribute to them
+          </p>
+        </a>
+
+        <a
+          href="https://github.com/misterioso013/alldevs/issues/new"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Feedback{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Tell us what you think about our projects
+          </p>
+        </a>
+      </div>
+    </main>
+  )
+}
